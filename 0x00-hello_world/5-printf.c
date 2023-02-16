aa08e94d5f2615b50a9840c5ff34241362199558
agnesimbs <agneswambui504@gmail.com>
2023-02-16 02:34:05 -0800

    uses printf to print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function entry point
+ * Description: using printf to print the output
+ * Return: 0
+ */
+int main(void)
+{
+	char a[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s", a);
+	return (0);
+}
