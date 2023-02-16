d3b5b92ab460e8f26c52b18494f75d1c32402200
agnesimbs <agneswambui504@gmail.com>
2023-02-16 02:28:55 -0800

    a hello code using puts to output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Main function entry point
+ * Description:uses puts to print the output
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
